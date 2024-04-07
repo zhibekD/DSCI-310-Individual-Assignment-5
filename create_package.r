d5e108568f155d0ca04abde81b1e8eddb4ad7b13
Zhibek D <d26zhibek@gmail.com>
2024-04-06 22:59:12 -0700

    Initial commit

--- create_package.r
+++ create_package.r
@@ -3,25 +3,3 @@ packageVersion("devtools")
 
 usethis::create_package("regexcite")
 use_git()
-
-
-(x <- "alfa,bravo,charlie,delta")
-strsplit(x, split = ",")
-
-str(strsplit(x, split = ","))
-
-unlist(strsplit(x, split = ","))
-
-strsplit1 <- function(x, split) {
-  strsplit(x, split = split)[[1]]
-}
-
-use_r("strsplit1")
-
-load_all()
-
-(x <- "alfa,bravo,charlie,delta")
-
-strsplit1(x, split = ",")
-
-exists("strsplit1", where = globalenv(), inherits = FALSE)
