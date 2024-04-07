47694cdab75673f864a008a18826b4df4173a523
Zhibek D <d26zhibek@gmail.com>
2024-04-06 21:46:40 -0700

    Initial commit

--- create_package.r
+++ create_package.r
@@ -7,3 +7,21 @@ use_git()
 
 (x <- "alfa,bravo,charlie,delta")
 strsplit(x, split = ",")
+
+str(strsplit(x, split = ","))
+
+unlist(strsplit(x, split = ","))
+
+strsplit1 <- function(x, split) {
+  strsplit(x, split = split)[[1]]
+}
+
+use_r("strsplit1")
+
+load_all()
+
+(x <- "alfa,bravo,charlie,delta")
+
+strsplit1(x, split = ",")
+
+exists("strsplit1", where = globalenv(), inherits = FALSE)
